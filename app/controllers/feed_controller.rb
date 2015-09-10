8c15cfb8db33a86f8d94b2ea85e2b97019714f79
Michael Dorrance <dorrancemd@gmail.com>
2015-09-10 13:43:43 -0600

    added pagination with 30+ images

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,8 +2,13 @@ class FeedController < ApplicationController
   before_action :authorize!
 
   def index
+    @feed = current_user.client.user_media_feed({count:100}).paginate(page: params[:page], per_page: 20)
   end
 
   def show
+  end
+
+  def search
+
   end
 end
